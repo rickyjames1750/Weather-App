99bae0755b30ab5b224762b713c161af445e3515
rickyjames1750 <rickyjames1750@gmail.com>
2020-08-08 18:52:02 -0700

    Initial Commit

---
+++ Weather App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Weather App
+//
+//  Created by Ricky Sparks on 8/8/20.
+//  Copyright © 2020 Ricky J. Sparks. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
