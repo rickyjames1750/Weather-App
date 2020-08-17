2bfb645fedf9c0f0fa31ff26cf4fcc9992f6eeec
rickyjames1750 <rickyjames1750@gmail.com>
2020-08-16 22:26:00 -0700

    This commit will do a request to a web api using Alamofire

--- Weather App/ViewController.swift
+++ Weather App/ViewController.swift
@@ -12,7 +12,7 @@ import SwiftyJSON
 import NVActivityIndicatorView
 import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
 
     @IBOutlet weak var locationLabel: UILabel!
     @IBOutlet weak var dayLabel: UILabel!
@@ -25,13 +25,49 @@ class ViewController: UIViewController {
     
     let gradientLayer = CAGradientLayer()
     
+    let apikey = "d3660d3f7146ff61ea9025056ee800d5"
+    var lat =  11.344533
+    var lon = 104.33322
+    var activityIndicator: NVActivityIndicatorView!
+    let locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         backgroundView.layer.addSublayer(gradientLayer)
+        
+        let indicatorSize:CGFloat = 70
+        let indicatorFrame = CGRect(x: (view.frame.width-indicatorSize)/2, y: (view.frame.height-indicatorSize)/2, width: indicatorSize, height: indicatorSize)
+        activityIndicator = NVActivityIndicatorView(frame: indicatorFrame, type: .lineScale, color: UIColor.white, padding: 20.0)
+        activityIndicator.backgroundColor = UIColor.black
+        view.addSubview(activityIndicator)
+        
+        activityIndicator.stopAnimating()
+        
+        locationManager.requestWhenInUseAuthorization()
+        activityIndicator.startAnimating()
+        if(CLLocationManager.locationServicesEnabled()) {
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+            locationManager.startUpdatingLocation()
+        }
+        
     }
     
     override func viewWillAppear(_ animated: Bool) {
         setBlueGradientBackground()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = locations[0]
+        lat = location.coordinate.latitude
+        lon = location.coordinate.longitude
+        Alamofire.request("https://api.openweathermap.org/data/2.5/weather?lat=\(lat)&lon=\(lon)&appid=\(apikey)&units=metric").responseJSON {
+            response in
+            self.activityIndicator.stopAnimating()
+            if let responseStr = response.result.value {
+                let jsonResponse = J
+            }
+        }
     }
 
     func setBlueGradientBackground() {
